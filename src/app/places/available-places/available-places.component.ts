db414a94686186c73be71da3af4b76b4d08e59a1
Ram Kumar <ramgambit@gmail.com>
2025-06-08 20:58:09 +0530

    sending http get request

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
@@ -12,7 +12,12 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
   private httpClient = inject(HttpClient);
+  ngOnInit() {
+    this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places')
+      .subscribe((resp) => this.places.set(resp.places));
+  }
 }
