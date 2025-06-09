105f8f538a830963a52b9470a903cad60eeab12a
Ram Kumar <ramgambit@gmail.com>
2025-06-09 14:15:50 +0530

    implemented dynamic update of user places

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -1,17 +1,22 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { DestroyRef, inject, Injectable, signal } from '@angular/core';
 
 import { Place } from './place.model';
 import { HttpClient } from '@angular/common/http';
-import { catchError, map, throwError } from 'rxjs';
+import { catchError, map, throwError, Subscription, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlacesService {
   private userPlaces = signal<Place[]>([]);
+  private userPlacesError = signal<string | null>(null);
+  private userPlacesLoading = signal<boolean>(false);
   private baseUrl = 'http://localhost:3000/';
   private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
   loadedUserPlaces = this.userPlaces.asReadonly();
+  userPlacesError$ = this.userPlacesError.asReadonly();
+  userPlacesLoading$ = this.userPlacesLoading.asReadonly();
 
   loadAvailablePlaces() {
     return this.fetchPlaces(
@@ -21,14 +26,35 @@ export class PlacesService {
   }
 
   loadUserPlaces() {
-    return this.fetchPlaces(
+    this.userPlacesLoading.set(true);
+    const subscription = this.fetchPlaces(
       'user-places',
       'Some thing went wrong fetching your places. Please try again later.'
-    );
+    ).subscribe({
+      next: (places) => {
+        this.userPlaces.set(places);
+      },
+      error: (err) => {
+        this.userPlacesLoading.set(false);
+        this.userPlacesError.set(err);
+      },
+      complete: () => {
+        this.userPlacesLoading.set(false);
+      },
+    });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
   }
 
   addPlaceToUserPlaces(placeId: string) {
-    return this.httpClient.put(this.baseUrl + 'user-places', { placeId });
+    return this.httpClient.put(this.baseUrl + 'user-places', { placeId }).pipe(
+      tap({
+        next: () => {
+          this.loadUserPlaces();
+        },
+      })
+    );
   }
 
   removeUserPlace(place: Place) {}
--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
@@ -13,29 +13,13 @@ import { Subscription } from 'rxjs';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
-  places = signal<Place[] | undefined>(undefined);
-  isFetching = signal<boolean>(false);
-  error = signal<string | null>(null);
-  private destroyRef = inject(DestroyRef);
+export class UserPlacesComponent implements OnInit {
   private placesService = inject(PlacesService);
-  private closeSubscription(subscription: Subscription): void {
-    if (subscription) {
-      this.destroyRef.onDestroy(() => {
-        subscription.unsubscribe();
-      });
-    }
-  }
+  places = this.placesService.loadedUserPlaces;
+  error = this.placesService.userPlacesError$;
+  isFetching = this.placesService.userPlacesLoading$;
+
   ngOnInit() {
-    this.isFetching.set(true);
-    const subscription = this.placesService.loadUserPlaces().subscribe({
-      next: (resp) => this.places.set(resp),
-      complete: () => this.isFetching.set(false),
-      error: (err) => {
-        this.isFetching.set(false);
-        this.error.set(err);
-      },
-    });
-    this.closeSubscription(subscription);
+    this.placesService.loadUserPlaces();
   }
 }
